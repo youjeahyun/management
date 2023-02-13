f32eff60de994d70d7c18b7d1ab66b9e4a07af28
youjeahyun <wogusdl9747@naver.com>
2023-02-13 17:24:38 +0900

    function방식 컴포넌트 구조화 및 map사용

--- src/App.js
+++ src/App.js
@@ -2,21 +2,52 @@ import logo from './logo.svg';
 import './App.css';
 import * as Customer from './components/Customer'; // 함수
 
-const customer = {
+const customers = [
+  {
+  'id':1,
+  'image': 'https://placeimg.com/64/64/any',
   'name':'유재현',
   'birthday':'19941230',
   'gender':'남성',
   'job':'개발자',
+},
+{
+  'id':2,
+  'image': 'https://placeimg.com/82/82/1',
+  'name':'김하림',
+  'birthday':'19970910',
+  'gender':'여성',
+  'job':'운동강사',
+},
+{
+  'id':3,
+  'image': 'https://placeimg.com/42/42/2',
+  'name':'이동혁',
+  'birthday':'19940403',
+  'gender':'남성',
+  'job':'서버관리자',
 }
+]
 
 function App() {
   return (
-   <Customer.Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}      
-   />
+   <div>
+   {
+    customers.map(c => {
+      return (
+          <Customer.Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+      );
+    })
+   }
+   </div>
   );
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -2,10 +2,31 @@
 export function Customer(props) {
     return (
       <div>
-        <h2>{props.name}</h2>
-        <p>{props.birthday}</p>
-        <p>{props.gender}</p>
-        <p>{props.job}</p>
+         <CustomerProfile
+            id={props.id} image={props.image} name={props.name}
+         />
+         <CustomerInfo
+            birthday={props.birthday} gender={props.gender} job={props.job}
+         />
       </div>
     );
-  }
+}
+
+function CustomerProfile(props){
+    return (
+        <div>
+            <img src={props.image} alt="profile"/>
+            <h2>{props.name}({props.id})</h2>
+        </div>
+      );
+}
+
+function CustomerInfo(props){
+    return (
+        <div>
+            <p>{props.birthday}</p>
+            <p>{props.gender}</p>
+            <p>{props.job}</p>
+        </div>
+      );
+}
