0479df66cf43465a4e3669f86514605b2427f13a
youjeahyun <wogusdl9747@naver.com>
2023-02-14 16:28:57 +0900

    material ui 테이블 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import * as Customer from './components/Customer'; // 함수
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { styled } from '@mui/material/styles';
+import Paper from '@material-ui/core/Paper';
 
 const customers = [
   {
@@ -13,7 +20,7 @@ const customers = [
 },
 {
   'id':2,
-  'image': 'https://placeimg.com/82/82/1',
+  'image': 'https://placeimg.com/64/64/1',
   'name':'김하림',
   'birthday':'19970910',
   'gender':'여성',
@@ -21,7 +28,7 @@ const customers = [
 },
 {
   'id':3,
-  'image': 'https://placeimg.com/42/42/2',
+  'image': 'https://placeimg.com/64/64/2',
   'name':'이동혁',
   'birthday':'19940403',
   'gender':'남성',
@@ -31,23 +38,39 @@ const customers = [
 
 function App() {
   return (
-   <div>
-   {
-    customers.map(c => {
-      return (
-          <Customer.Customer
-            key={c.id}
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birthday={c.birthday}
-            gender={c.gender}
-            job={c.job}
-          />
-      );
-    })
-   }
-   </div>
+   <Paper className='root'>
+      <Table className='table'>     
+        <TableHead  >
+          <TableRow>         
+             <TableCell>번호</TableCell>
+             <TableCell>이미지</TableCell>
+             <TableCell>이름</TableCell>
+             <TableCell>생년월일</TableCell>
+             <TableCell>성별</TableCell>
+             <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {
+          customers.map(c => {
+            return (
+                
+                    <Customer.Customer
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />
+                
+            );
+          })
+        }
+        </TableBody>
+    </Table>
+   </Paper>
   );
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,33 +1,15 @@
-import TableRow from '@material-ui/core/TableRow'
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell'
 
 export function Customer(props) {
     return (
-      <div>
-         <CustomerProfile
-            id={props.id} image={props.image} name={props.name}
-         />
-         <CustomerInfo
-            birthday={props.birthday} gender={props.gender} job={props.job}
-         />
-      </div>
+        <TableRow>
+            <TableCell>{props.id}</TableCell>
+            <TableCell><img src={props.image} alt="profile"/></TableCell>
+            <TableCell>{props.name}</TableCell>
+            <TableCell>{props.birthday}</TableCell>
+            <TableCell>{props.gender}</TableCell>
+            <TableCell>{props.job}</TableCell>
+        </TableRow>
     );
-}
-
-function CustomerProfile(props){
-    return (
-        <div>
-            <img src={props.image} alt="profile"/>
-            <h2>{props.name}({props.id})</h2>
-        </div>
-      );
-}
-
-function CustomerInfo(props){
-    return (
-        <div>
-            <p>{props.birthday}</p>
-            <p>{props.gender}</p>
-            <p>{props.job}</p>
-        </div>
-      );
 }
