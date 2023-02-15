6238921de53fb3f86c88943812db6038661838ff
youjeahyun <wogusdl9747@naver.com>
2023-02-15 16:35:24 +0900

    더미데이터 추가 // api호출까지 해서 프로젝트 마무리

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/api/customers', (req,res) =>{
     res.send([
         {
         'id':1,
-        'image': 'https://placeimg.com/64/64/any',
+        'image': 'https://placeimg.com/64/64/1',
         'name':'유재현',
         'birthday':'19941230',
         'gender':'남성',
@@ -16,7 +16,7 @@ app.get('/api/customers', (req,res) =>{
       },
       {
         'id':2,
-        'image': 'https://placeimg.com/64/64/1',
+        'image': 'https://placeimg.com/64/64/2',
         'name':'김하림',
         'birthday':'19970910',
         'gender':'여성',
@@ -24,11 +24,35 @@ app.get('/api/customers', (req,res) =>{
       },
       {
         'id':3,
-        'image': 'https://placeimg.com/64/64/2',
+        'image': 'https://placeimg.com/64/64/3',
         'name':'이동혁',
         'birthday':'19940403',
         'gender':'남성',
         'job':'서버관리자',
+      },
+      {
+        'id':4,
+        'image': 'https://placeimg.com/64/64/4',
+        'name':'김준석',
+        'birthday':'19940403',
+        'gender':'남성',
+        'job':'개발자',
+      },
+      {
+        'id':5,
+        'image': 'https://placeimg.com/64/64/5',
+        'name':'전태룡',
+        'birthday':'19940403',
+        'gender':'남성',
+        'job':'개발자',
+      },
+      {
+        'id':6,
+        'image': 'https://placeimg.com/64/64/6',
+        'name':'최규민',
+        'birthday':'19940403',
+        'gender':'남성',
+        'job':'퍼블리셔',
       }
       ]);
 });
