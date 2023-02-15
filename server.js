4d11317766dd010dda056f58c722e14ffddb707f
youjeahyun <wogusdl9747@naver.com>
2023-02-15 15:46:17 +0900

    body-parser 제거 (express에 내재되있음)

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app =express();
 const port = process.env.PORT || 5000;
-const router = express.Router();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.get('/api/customers', (req,res) =>{
     res.send([
         {
